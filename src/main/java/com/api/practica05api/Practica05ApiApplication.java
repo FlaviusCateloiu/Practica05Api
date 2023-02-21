6b56fe921e75249fd6de4a3ff9279aa3a41d4e79
Flavius <fcateloiu08@gmail.com>
2023-02-21 15:04:23 +0100

    I am create the projecto with Spring.

---
+++ src/main/java/com/api/practica05api/Practica05ApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.practica05api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica05ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica05ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/practica05api/Practica05ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.practica05api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica05ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
